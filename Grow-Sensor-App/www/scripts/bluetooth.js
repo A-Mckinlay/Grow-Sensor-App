14b534fa469e5dbbd0f34ee33275c7270ae17ee0
Allan McKinlay <a.mckinlay@dundee.ac.uk>
2017-12-14 23:21:30 +0000

    hooked up front end to example

--- Grow-Sensor-App/www/scripts/bluetooth.js
+++ Grow-Sensor-App/www/scripts/bluetooth.js
@@ -310,3 +310,24 @@ function reportValue(serviceUuid, characteristicUuid, value) {
     document.getElementById(serviceUuid + "." + characteristicUuid).textContent = value;
 }
 
+function makeUL(array) {
+    // Create the list element:
+    var list = document.createElement('ul');
+
+    for (var i = 0; i < array.length; i++) {
+        // Create the list item:
+        var item = document.createElement('li');
+
+        // Set its contents:
+        item.appendChild(document.createTextNode(array[i]));
+
+        // Add it to the list:
+        list.appendChild(item);
+    }
+
+    // Finally, return the constructed list:
+    return list;
+}
+
+var btn = document.querySelector('start-scan');
+btn.onclick = startScan();
